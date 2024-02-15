26639bb07af86c97e162567c3002d1af10d3d835
Lucas Weiß <lucasweiss321123@live.de>
2024-02-15 14:18:39 +0100

    last commit

--- script.js
+++ script.js
@@ -2,46 +2,20 @@
 let rightQuestions = 0;
 let currentQuestion = 0;
 
+let AUDIO_SUCCESS = new Audio('audio/win.mp3');
+let AUDIO_FAIL = new Audio('audio/lose.mp3');
+
 function init() {
   document.getElementById("all-questions").innerHTML = questions.length;
   showQuestion();
 }
 
 function showQuestion() {
-
-
-    if (currentQuestion >= questions.length){
-      document.getElementById("end-container").style = '';
-      //entfernt den Style display none vom end-container
-      document.getElementById("question-container").style = "display: none!important";
-      //fügt den Style display none dem question-container hinzu
-
-      document.getElementById("amount-of-questions").innerHTML = questions.length; 
-      // zeigt im end-container an wie viele fragen vorhanden sind 
-      document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
-      //zeigt im end-container an wie viele fragen richtig sind  
-      
+    if (gameIsOver()){
+      showEndScreen();
     }else{
-      let percent = (currentQuestion + 1) / questions.length;
-      percent = Math.round(percent * 100);
-      document.getElementById("progress-bar").innerHTML = `${percent}%`
-      document.getElementById("progress-bar").style = `width: ${percent}%`
-
-        document.getElementById("current-question").innerHTML = currentQuestion + 1;
-        // zeigt im Element der id current-question die aktuelle Frage an (1 weil currentQuestion = 0, 0+1=1)
-        let question = questions[currentQuestion];
-        // die Variable currentQuestion hat den Wert 0 definiert
-        // geht in questions rein und nimmt den Wert 0(weil currentQuestion=0) heraus
-        document.getElementById("questiontext").innerHTML = question["question"];
-        //geht in questions[0]['question']und der wert wird bei id questiontext eingefügt("Wer hat HTML erfunden?")
-        document.getElementById("answer_1").innerHTML = question["answer_1"];
-        //geht in questions[0]['answer_1']und der wert wird bei id answer_1 eingefügt("Robbie Williams")
-        document.getElementById("answer_2").innerHTML = question["answer_2"];
-        //geht in questions[0]['answer_2']und der wert wird bei id answer_2 eingefügt("Lady Gaga")
-        document.getElementById("answer_3").innerHTML = question["answer_3"];
-        //geht in questions[0]['answer_3']und der wert wird bei id answer_3 eingefügt("Tim Berners-Lee")
-        document.getElementById("answer_4").innerHTML = question["answer_4"];
-        //geht in questions[0]['answer_4']und der wert wird bei id answer_4 eingefügt("Justin Bieber")
+      progressBar();
+      showNextQuestion();
     }
 }
 
@@ -54,17 +28,19 @@ function answer(selection) {
   let idOfRightAnswer = `answer_${question["right_answer"]}`;
   // es wird immer answer_(die richtig antwort einer frage (3))
 
-  if (selectedQuestionNumber == question["right_answer"]) {
+  if (rightAnswerSelected(selectedQuestionNumber, question)) {
       //Wenn selectedQuestionNumber(1) == 3
       document.getElementById(selection).parentNode.classList.add("bg-success");
       //dann parentElement der id selection(answer_3) die Klasse bg-success hinzufügen
       rightQuestions++;
       // fügt der Variable +1 hinzu 
+      AUDIO_SUCCESS.play();
   } else {
     //ansonsten parentElement der id selection(answer_1/answer_2/answer_4) die Klasse bg-danger hinzufügen
     //& parentElement der id idOfRightAnswer(answer_3) die Klasse bg-success hinzufügen
     document.getElementById(selection).parentNode.classList.add("bg-danger");
     document.getElementById(idOfRightAnswer).parentNode.classList.add("bg-success");
+    AUDIO_FAIL.play()
   }
   document.getElementById("next-btn").disabled = false;
   //wenn eine Antwort ausgewählt dann wird der Button freigegeben bzw enabled
@@ -91,3 +67,61 @@ function resetAnswerButtons(){
     document.getElementById("answer_4").parentElement.classList.remove('bg-success');
 }
 
+function replay() {
+  rightQuestions = 0;
+  currentQuestion = 0;
+  init();
+  document.getElementById("end-container").style = 'display: none!important';
+  //fügt den Style display none dem end-container hinzu
+  document.getElementById("question-container").style = "";
+  //entfernt den Style display none vom question-container
+}
+
+
+
+function showEndScreen(){
+  document.getElementById("end-container").style = '';
+  //entfernt den Style display none vom end-container
+  document.getElementById("question-container").style = "display: none!important";
+  //fügt den Style display none dem question-container hinzu
+
+  document.getElementById("amount-of-questions").innerHTML = questions.length; 
+  // zeigt im end-container an wie viele fragen vorhanden sind 
+  document.getElementById("amount-of-right-questions").innerHTML = rightQuestions;
+  //zeigt im end-container an wie viele fragen richtig sind  
+}
+
+
+function showNextQuestion(){
+        document.getElementById("current-question").innerHTML = currentQuestion + 1;
+        // zeigt im Element der id current-question die aktuelle Frage an (1 weil currentQuestion = 0, 0+1=1)
+        let question = questions[currentQuestion];
+        // die Variable currentQuestion hat den Wert 0 definiert
+        // geht in questions rein und nimmt den Wert 0(weil currentQuestion=0) heraus
+        document.getElementById("questiontext").innerHTML = question["question"];
+        //geht in questions[0]['question']und der wert wird bei id questiontext eingefügt("Wer hat HTML erfunden?")
+        document.getElementById("answer_1").innerHTML = question["answer_1"];
+        //geht in questions[0]['answer_1']und der wert wird bei id answer_1 eingefügt("Robbie Williams")
+        document.getElementById("answer_2").innerHTML = question["answer_2"];
+        //geht in questions[0]['answer_2']und der wert wird bei id answer_2 eingefügt("Lady Gaga")
+        document.getElementById("answer_3").innerHTML = question["answer_3"];
+        //geht in questions[0]['answer_3']und der wert wird bei id answer_3 eingefügt("Tim Berners-Lee")
+        document.getElementById("answer_4").innerHTML = question["answer_4"];
+        //geht in questions[0]['answer_4']und der wert wird bei id answer_4 eingefügt("Justin Bieber")
+}
+
+function progressBar(){
+  let percent = (currentQuestion + 1) / questions.length;
+      percent = Math.round(percent * 100);
+      document.getElementById("progress-bar").innerHTML = `${percent}%`;
+      document.getElementById("progress-bar").style = `width: ${percent}%`;
+}
+
+
+function gameIsOver(){
+  return currentQuestion >= questions.length;
+}
+
+function rightAnswerSelected(selectedQuestionNumber, question){
+  return selectedQuestionNumber == question["right_answer"];
+}
